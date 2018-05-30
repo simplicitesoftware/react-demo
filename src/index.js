4cab38d1bf0ebaf8e9862671822bdeebddec8242
David AZOULAY <dazoulay@simplicite.fr>
2018-05-30 11:48:45 +0200

    Milestone

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Demo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.url = props.url || "https://demo.dev.simplicite.io/api";
+		this.username = props.username || "designer";
+		this.password = props.password || "designer";
+	}
+
+	render() {
+		return (
+			<div>
+				URL: {this.url}<br/>
+				Username/password: {this.username}/{this.password}
+			</div>
+		);
+
+	}
+};
+
 ReactDOM.render(
-  <h1>Hello, Simplicit&eacute; world!</h1>,
-  document.getElementById('root')
+	<Demo username="website" password="simplicite"/>,
+	document.getElementById('root')
 );
