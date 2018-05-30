59dde844789d240fbe8b089878664dca528afa46
David AZOULAY <dazoulay@simplicite.fr>
2018-05-30 16:09:06 +0200

    Get grant data

--- src/index.js
+++ src/index.js
@@ -2,26 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function _loadScript(src, callback) {
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	script.async = true
+	script.src = src;
+	script.onload = callback;
+	document.head.appendChild(script);
+}
+
 class Demo extends React.Component {
 	constructor(props) {
 		super(props);
-		this.url = props.url || "https://demo.dev.simplicite.io/api";
-		this.username = props.username || "designer";
-		this.password = props.password || "designer";
+		this.state = {};
+	}
+
+	componentWillMount() {
+		let self = this;
+		_loadScript(self.props.url + "/scripts/ajax/bundle.js", function() {
+			let app = new window.Simplicite.Ajax(self.props.url, "api", self.props.username, self.props.password);
+			console.log(app);
+			self.setState(app);
+			app.getGrant(function() {
+				console.log(app.grant);
+				console.log("Signed in as " + app.grant.login);
+				self.setState(app);
+			});
+		});
 	}
 
 	render() {
 		return (
-			<div>
-				URL: {this.url}<br/>
-				Username/password: {this.username}/{this.password}
-			</div>
+			<div>{ this.state.grant && this.state.grant.login  ? "Hello " + this.state.grant.login + "!" : "(Loading...)" }</div>
 		);
 
 	}
 };
 
 ReactDOM.render(
-	<Demo username="website" password="simplicite"/>,
+	<Demo url="https://dev40.dev.simplicite.io" username="website" password="simplicite"/>,
 	document.getElementById('root')
 );
