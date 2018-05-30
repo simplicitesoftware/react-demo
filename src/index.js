d267559d2d0a957090e1685925378973382f3722
David AZOULAY <dazoulay@simplicite.fr>
2018-05-30 17:13:09 +0200

    Comments

--- src/index.js
+++ src/index.js
@@ -19,15 +19,18 @@ class Demo extends React.Component {
 		}
 
 		let self = this;
+		// Asynchronously load the Ajax lib bundle...
 		loadScript(self.props.url + "/scripts/ajax/bundle.js", function() {
+			// ...then instanciate the app from the properties...
 			let app = new window.Simplicite.Ajax(self.props.url, "api", self.props.username, self.props.password);
+			// ...then load the user grant...
 			app.getGrant(function() {
-				self.setState(app);
+				self.setState(app); // Set state to trigger render
+				// ...then search for products
 				let prd = app.getBusinessObject("DemoProduct");
 				prd.search(function() {
-					app.products = prd.list;
-					console.log(app.products);
-					self.setState(app);
+					app.products = prd.list; // Put the search result in an "easy(ier)" location in state object
+					self.setState(app); // Set state to trigger render
 				});
 			});
 		});
@@ -48,6 +51,6 @@ class Demo extends React.Component {
 };
 
 ReactDOM.render(
-	<Demo url="https://dev40.dev.simplicite.io" username="website" password="simplicite"/>,
+	<Demo url="https://demo.dev.simplicite.io" username="website" password="simplicite"/>,
 	document.getElementById('root')
 );
