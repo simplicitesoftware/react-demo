1537f1a30e40fade3576efd0e71d5b7b8f2c8a07
David AZOULAY <dazoulay@simplicite.fr>
2018-05-30 11:05:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Simplicite + React demo
+=======================
+
+Run: `npm start`
+
+Build: `npm run build`
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <h1>Hello, Simplicit&eacute; world!</h1>,
+  document.getElementById('root')
+);
